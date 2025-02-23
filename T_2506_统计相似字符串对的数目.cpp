a54fd6423074d4a7963d32e4f43d0c19fa081071
BruceLee <li200512182022@126.com>
2025-02-23 20:40:02 +0800

    master

---
+++ T_2506_统计相似字符串对的数目.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+class Solution {
+    public:
+        int similarPairs(vector<string>& words) {
+            int res = 0;
+            unordered_map<int, int> cnt;
+            for (const string& word : words) {
+                int state = 0;
+                for (char c : word) {
+                    state |= 1 << (c - 'a');
+                }
+                res += cnt[state];
+                cnt[state]++;
+            }
+            return res;
+        }
+};
+
